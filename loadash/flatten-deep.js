cdec86dffc05744b304778f92f15a0c40c137dce
Vakadu Vinod <vakadu10@gmail.com>
2025-07-14 12:51:43 +0530

    new changes

--- loadash/flatten-deep.js
+++ loadash/flatten-deep.js
@@ -1,9 +1,9 @@
-function customFlattenDeep(arr) {
+function customFlattenDeep(arr, depth) {
     let result = [];
 
     for (const element of arr) {
-        if(Array.isArray(element)) {
-            result.push(...customFlattenDeep(element))
+        if(Array.isArray(element) && depth > 0) {
+            result.push(...customFlattenDeep(element, depth - 1))
         } else {
             result.push(element);
         }
@@ -12,6 +12,9 @@ function customFlattenDeep(arr) {
     return result
 }
 
-const arr = [1, [2, [3, [4]]]];
-console.log(customFlattenDeep(arr));  // Output: [1, 2, 3, 4]
+// const arr = [1, [2, [3, [4]]]];
+// console.log(customFlattenDeep(arr, 2));  // Output: [1, 2, 3, 4]
+
+const arr = [[1], [[2], [[3]]]];
+console.log(customFlattenDeep(arr, 3));  // Output: [1, 2, 3]
 
